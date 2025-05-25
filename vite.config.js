4353905ec8e4c0b4ef009a9f72753072af66e371
AwsAqh <awsaqh@gmail.com>
2025-05-25 22:17:12 +0300

    allow all domains

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['ptm-3.onrender.com']
+    allowedHosts: true
   }
 })
