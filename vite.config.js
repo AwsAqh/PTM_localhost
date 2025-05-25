0a350f4d63161c9c024ee3f4fed7771d2e79c517
AwsAqh <awsaqh@gmail.com>
2025-05-25 21:53:51 +0300

    allow render domain

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['ptm-3.onrender.com']
+  }
 })
